c9421a14072cb1e86e1551c288fc7401b388d632
andela-vkironde <victor.kironde@andela.com>
2016-10-19 11:32:33 +0300

    ft(test) add factory-girl gem

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,8 @@
-require 'rails_helper'
+require 'spec_helper'
 
-RSpec.describe User, type: :model do
-  let(:user) { User.new}
-  subject{ user }
-  it { should respond_to(:name) }
-  it { should respond_to(:email) }
-  it { should respond_to(:password) }
+describe User, type: :model do
+  
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:email) }
+  it { should validate_presence_of(:password) }
 end
