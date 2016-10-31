8a34da78e74b2add436eb761e161052adf505f7f
andela-vkironde <victor.kironde@andela.com>
2016-10-31 11:58:44 +0300

    Add authentication to user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161030005148) do
+ActiveRecord::Schema.define(version: 20161031082557) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,8 @@ ActiveRecord::Schema.define(version: 20161030005148) do
     t.integer  "reference"
     t.datetime "date"
     t.string   "email"
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id", using: :btree
   end
 
   create_table "flights", force: :cascade do |t|
@@ -60,9 +62,10 @@ ActiveRecord::Schema.define(version: 20161030005148) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.string   "password"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
+  add_foreign_key "bookings", "users"
 end
