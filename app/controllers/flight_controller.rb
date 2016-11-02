afc730633d602c863d406130e65324d6eb0819df
andela-vkironde <victor.kironde@andela.com>
2016-11-02 12:53:03 +0300

    ch(models): add flight seed data

--- app/controllers/flight_controller.rb
+++ app/controllers/flight_controller.rb
@@ -1,11 +1,23 @@
 class FlightController < ApplicationController
 
+  attr_reader :errors, :flights, :passengers, :date
+
   def index
     @flight = Flight.new
     @airports = Airport.airports_all
   end
-
-  def search_flight
-
+  def search_flights
+    @flights = Flight.search_flights(flight_params)
+    flash[:notice] = nil
+    if @flights.empty?
+      flash[:notice] = "There are no available flights."
+      respond_to do |format|
+        format.js {render layout: false}
+      end
+    else
+      respond_to do |format|
+        format.js {render layout: false}
+      end
+    end
   end
 end
