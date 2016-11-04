2bbafac953f26447bc3675675142e9fbcbf9a60f
andela-vkironde <victor.kironde@andela.com>
2016-11-04 14:55:46 +0300

    Add past bookings

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,9 +1,5 @@
 class Passenger < ApplicationRecord
-  validates :name, :passport, presence: true
+  validates :name, :passport_number, presence: true
   belongs_to :booking, inverse_of: :passengers
   validates_presence_of :booking
-  validates :age,
-            presence: true,
-            numericality: { greater_than_or_equal_to: 0,
-            only_integer: true }
 end
