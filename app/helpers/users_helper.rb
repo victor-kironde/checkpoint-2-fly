d2943e0dc14b51f79df156d217dcde9dea277257
andela-vkironde <victor.kironde@andela.com>
2016-11-04 14:55:46 +0300

    ft(views): add past bookings page

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,7 @@
 module UsersHelper
+  def booking_expired(booking)
+    p booking.flight.departure_date(booking.flight.departure)
+    p Time.now
+    booking.flight.departure_date(booking.departure) < Time.now
+  end
 end
