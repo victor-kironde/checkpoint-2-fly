25d0d98937f3fa003003fd29ab1fe208d38f5d23
andela-vkironde <victor.kironde@andela.com>
2016-11-07 09:55:08 +0300

    ch(models):remove repeated migrations

--- db/migrate/20161103095922_refactor_flights.rb
+++ db/migrate/20161103095922_refactor_flights.rb
@@ -1,7 +1,5 @@
 class RefactorFlights < ActiveRecord::Migration[5.0]
   def change
     remove_column :flights, :airline
-    remove_column :flights, :flight_number
-    remove_column :flights, :date
   end
 end
