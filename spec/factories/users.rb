7601d367bc4dd7d41b8839b0a11cfc7161754938
andela-vkironde <victor.kironde@andela.com>
2016-11-08 15:25:36 +0300

    ft(tests): add feature tests for users

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,9 @@
 FactoryGirl.define do
   factory :user do
-    sequence(:name) { |i| "User #{i}"}
-    sequence(:email) { |i| "user#{i}@example.com"}
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    email { Faker::Internet.email }
     password 'password'
+    password_confirmation 'password'
   end
 end
---
+++ spec/features/user_signup_spec.rb
@@ -0,0 +1,51 @@
+require 'rails_helper'
+
+RSpec.feature 'Sign Up', js: true do
+  before(:all) do
+    @user = build(:user)
+    @registered = create(:user)
+  end
+
+  scenario 'User clicks on Register' do
+    visit root_path
+    click_on 'Sign Up'
+    expect(page).to have_content('SIGN UP')
+    expect(page).to have_content('First name')
+    expect(page).to have_content('Last name')
+    expect(page).to have_content('Email')
+  end
+
+  scenario 'User signs up with valid Credentials' do
+    signup(@user.first_name, @user.last_name, @user.email, @user.password,
+           @user.password_confirmation)
+    expect(page).to have_content('Log Out')
+    expect(page).to have_content("Welcome #{@user.first_name}")
+  end
+
+  scenario 'User enters a used email' do
+    signup(@user.first_name, @user.last_name, @registered.email, @user.password,
+           @user.password_confirmation)
+    expect(page).to have_content('Email has already been taken')
+  end
+
+  scenario 'User enters no data at all' do
+    signup('', '', '', '', '')
+    expect(page).to have_content('errors found')
+    expect(page).to have_content("First name can't be blank")
+    expect(page).to have_content("Last name can't be blank")
+    expect(page).to have_content("Password can't be blank")
+    expect(page).to have_content("Password confirmation can't be blank")
+    expect(page).to have_content("Email can't be blank")
+    expect(page).to have_content('Email is invalid')
+  end
+
+  def signup(first_name, last_name, email, password, password_confirmation)
+    visit signup_path
+    fill_in 'firstname', with: first_name
+    fill_in 'lastname', with: last_name
+    fill_in 'email', with: email
+    fill_in 'password', with: password
+    fill_in 'confirm', with: password_confirmation
+    click_button 'Register'
+  end
+end
