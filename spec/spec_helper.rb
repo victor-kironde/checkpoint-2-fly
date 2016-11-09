cd28b3a6312d365daa48a3d2666511b82b2be6fa
andela-vkironde <victor.kironde@andela.com>
2016-11-09 10:24:57 +0300

    ch() Integrate Coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,8 @@
+require 'coveralls'
+Coveralls.wear!
 require 'rails_helper'
 require "capybara/rspec"
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
