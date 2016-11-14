91b95fd7cb084dc356aa220c329b235663ab5eaa
andela-vkironde <victor.kironde@andela.com>
2016-11-14 15:37:14 +0300

    ch(): refactor error messages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,16 +7,17 @@ class SessionsController < ApplicationController
     if @user && @user.authenticate(params[:session][:password])
       session[:user_id] = @user.id
       redirect_to root_path
-      flash[:success] = "Welcome back, #{@user.full_name}"
+      flash[:success] = log_in_success(@user)
     else
-      flash[:danger] = "Incorrect email or password."
+      flash[:danger] = log_in_error
       redirect_to login_path
     end
   end
 
   def destroy
+    name = current_user.full_name
     session[:user_id] = nil
     redirect_to root_path
-    flash[:success] = "You have successfully Logged out"
+    flash[:success] = log_out_success
   end
 end
