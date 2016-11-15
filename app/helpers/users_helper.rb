59d0abd54693de33b4481f9f0f838a22c1779594
andela-vkironde <victor.kironde@andela.com>
2016-11-15 16:29:12 +0300

    ch(views): add user helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,2 @@
 module UsersHelper
-  def booking_expired(booking)
-    booking.flight.departure_date(booking.flight.departure)
-    Time.now
-    booking.flight.departure_date(booking.departure) < Time.now
-  end
 end
