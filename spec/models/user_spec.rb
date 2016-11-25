815ddc8edd9a4f4d4e9d864401da4c4e3f7a0b3f
andela-vkironde <victor.kironde@andela.com>
2016-11-25 11:07:46 +0300

    ch(test): refactor user model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,8 @@
 require "spec_helper"
 
 describe User, type: :model do
-  it { should validate_presence_of(:first_name) }
-  it { should validate_presence_of(:last_name) }
-  it { should validate_presence_of(:email) }
-  it { should validate_presence_of(:password) }
+  it { is_expected.to validate_presence_of(:first_name) }
+  it { is_expected.to validate_presence_of(:last_name) }
+  it { is_expected.to validate_presence_of(:email) }
+  it { is_expected.to validate_presence_of(:password) }
 end
