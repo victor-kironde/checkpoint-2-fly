45392852090d4663ce85af1cee70f9f15d30777e
andela-vkironde <victor.kironde@andela.com>
2016-11-30 12:31:09 +0300

    ft(views): make ui more responisve

--- app/assets/javascripts/table.js
+++ app/assets/javascripts/table.js
@@ -1,9 +1,10 @@
 // '.tbl-content' consumed little space for vertical scrollbar, scrollbar width depend on browser/os/platfrom. Here calculate the scollbar width .
 $(window).on("load resize ", function() {
-  var scrollWidth = $('.tbl-content').width() - $('.tbl-content table').width();
+  var scrollWidth = $('.tbl-content').width() - $('.tbl-content').width();
   $('.tbl-header').css({'padding-right':scrollWidth});
 }).resize();
 
+//error fade
 $(".alert").fadeTo(2000, 500).slideUp(500, function(){
     $(".alert").slideUp(500);
 });
